24e06b25334759522c4bca343f4b2238b6752432
Axel Gael Carlin Guzman <axel.18hrs@gmail.com>
2023-02-25 10:08:36 -0600

    Se hicieron bastantes cambios desde la ultima version subida a github, esta vez me enfoque en mejorar la "interfaz de usuario" para continuar despues con unos ajustes y extras en el codigo.

--- WindowsForms_VB/Form1.Designer.vb
+++ WindowsForms_VB/Form1.Designer.vb
@@ -22,92 +22,235 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.txtNumeroTelefono = New System.Windows.Forms.TextBox()
+        Me.btnEnviarDatos = New System.Windows.Forms.Button()
+        Me.ErrorProvider1 = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.ErrorProvider2 = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.ErrorProvider3 = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.txtCantidadContactos = New System.Windows.Forms.TextBox()
+        Me.btnCantidadContactos = New System.Windows.Forms.Button()
+        Me.dgvDatosFormulario = New System.Windows.Forms.DataGridView()
+        Me.ID = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.NOMBRE = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.NumeroDeTelefono = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.FechaDeNacimiento = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Edad = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ErrorProvider4 = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.dtpFechaNacimiento = New System.Windows.Forms.DateTimePicker()
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ErrorProvider2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ErrorProvider3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        CType(Me.dgvDatosFormulario, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ErrorProvider4, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.Label1.Location = New System.Drawing.Point(369, 124)
+        Me.Label1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(119, 87)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(44, 13)
+        Me.Label1.Size = New System.Drawing.Size(65, 20)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Nombre"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.Label2.Location = New System.Drawing.Point(342, 175)
+        Me.Label2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(119, 123)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(100, 13)
+        Me.Label2.Size = New System.Drawing.Size(149, 20)
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "Numero de telefono"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.Label3.Location = New System.Drawing.Point(301, 225)
+        Me.Label3.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(119, 157)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(187, 13)
+        Me.Label3.Size = New System.Drawing.Size(157, 20)
         Me.Label3.TabIndex = 2
-        Me.Label3.Text = "Fecha de nacimiento - AAAA/MM/DD"
+        Me.Label3.Text = "Fecha de nacimiento"
         '
-        'TextBox1
+        'txtNombre
         '
-        Me.TextBox1.Location = New System.Drawing.Point(268, 140)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(249, 20)
-        Me.TextBox1.TabIndex = 3
+        Me.txtNombre.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNombre.Location = New System.Drawing.Point(294, 84)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(249, 26)
+        Me.txtNombre.TabIndex = 3
         '
-        'TextBox2
+        'txtNumeroTelefono
         '
-        Me.TextBox2.Location = New System.Drawing.Point(268, 191)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(249, 20)
-        Me.TextBox2.TabIndex = 4
+        Me.txtNumeroTelefono.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNumeroTelefono.Location = New System.Drawing.Point(294, 120)
+        Me.txtNumeroTelefono.Name = "txtNumeroTelefono"
+        Me.txtNumeroTelefono.Size = New System.Drawing.Size(249, 26)
+        Me.txtNumeroTelefono.TabIndex = 4
         '
-        'TextBox3
+        'btnEnviarDatos
         '
-        Me.TextBox3.Location = New System.Drawing.Point(268, 241)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(249, 20)
-        Me.TextBox3.TabIndex = 5
+        Me.btnEnviarDatos.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.btnEnviarDatos.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.btnEnviarDatos.Location = New System.Drawing.Point(562, 139)
+        Me.btnEnviarDatos.Name = "btnEnviarDatos"
+        Me.btnEnviarDatos.Size = New System.Drawing.Size(135, 43)
+        Me.btnEnviarDatos.TabIndex = 6
+        Me.btnEnviarDatos.Text = "Enviar datos"
+        Me.btnEnviarDatos.UseVisualStyleBackColor = False
         '
-        'Button1
+        'ErrorProvider1
         '
-        Me.Button1.BackColor = System.Drawing.Color.White
-        Me.Button1.ForeColor = System.Drawing.SystemColors.ControlText
-        Me.Button1.Location = New System.Drawing.Point(322, 282)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(135, 43)
-        Me.Button1.TabIndex = 6
-        Me.Button1.Text = "Enviar datos"
-        Me.Button1.UseVisualStyleBackColor = False
+        Me.ErrorProvider1.ContainerControl = Me
+        '
+        'ErrorProvider2
+        '
+        Me.ErrorProvider2.ContainerControl = Me
+        '
+        'ErrorProvider3
+        '
+        Me.ErrorProvider3.ContainerControl = Me
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ErrorProvider3.SetIconAlignment(Me.Label5, System.Windows.Forms.ErrorIconAlignment.BottomLeft)
+        Me.Label5.Location = New System.Drawing.Point(187, 158)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(423, 25)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "Ingresa una cantidad de perfiles para crear"
+        Me.Label5.UseMnemonic = False
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.txtCantidadContactos)
+        Me.Panel1.Controls.Add(Me.btnCantidadContactos)
+        Me.Panel1.Location = New System.Drawing.Point(3, 5)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(793, 441)
+        Me.Panel1.TabIndex = 12
+        '
+        'txtCantidadContactos
+        '
+        Me.txtCantidadContactos.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtCantidadContactos.Location = New System.Drawing.Point(270, 197)
+        Me.txtCantidadContactos.Name = "txtCantidadContactos"
+        Me.txtCantidadContactos.Size = New System.Drawing.Size(249, 26)
+        Me.txtCantidadContactos.TabIndex = 15
+        '
+        'btnCantidadContactos
+        '
+        Me.btnCantidadContactos.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.btnCantidadContactos.Location = New System.Drawing.Point(340, 229)
+        Me.btnCantidadContactos.Name = "btnCantidadContactos"
+        Me.btnCantidadContactos.Size = New System.Drawing.Size(113, 38)
+        Me.btnCantidadContactos.TabIndex = 16
+        Me.btnCantidadContactos.Text = "Confirmar"
+        Me.btnCantidadContactos.UseVisualStyleBackColor = False
+        '
+        'dgvDatosFormulario
+        '
+        Me.dgvDatosFormulario.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing
+        Me.dgvDatosFormulario.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.ID, Me.NOMBRE, Me.NumeroDeTelefono, Me.FechaDeNacimiento, Me.Edad})
+        Me.dgvDatosFormulario.Location = New System.Drawing.Point(117, 196)
+        Me.dgvDatosFormulario.Name = "dgvDatosFormulario"
+        Me.dgvDatosFormulario.Size = New System.Drawing.Size(580, 210)
+        Me.dgvDatosFormulario.TabIndex = 17
+        '
+        'ID
+        '
+        Me.ID.HeaderText = "ID"
+        Me.ID.Name = "ID"
+        Me.ID.ReadOnly = True
+        Me.ID.Width = 50
+        '
+        'NOMBRE
+        '
+        Me.NOMBRE.HeaderText = "Nombre"
+        Me.NOMBRE.Name = "NOMBRE"
+        Me.NOMBRE.ReadOnly = True
+        Me.NOMBRE.Width = 120
+        '
+        'NumeroDeTelefono
+        '
+        Me.NumeroDeTelefono.HeaderText = "Numero de telefono"
+        Me.NumeroDeTelefono.Name = "NumeroDeTelefono"
+        Me.NumeroDeTelefono.ReadOnly = True
+        Me.NumeroDeTelefono.Width = 120
+        '
+        'FechaDeNacimiento
+        '
+        Me.FechaDeNacimiento.HeaderText = "Fecha de nacimiento"
+        Me.FechaDeNacimiento.Name = "FechaDeNacimiento"
+        Me.FechaDeNacimiento.ReadOnly = True
+        Me.FechaDeNacimiento.Width = 120
+        '
+        'Edad
+        '
+        Me.Edad.HeaderText = "Edad"
+        Me.Edad.Name = "Edad"
+        Me.Edad.ReadOnly = True
+        Me.Edad.Width = 120
+        '
+        'ErrorProvider4
+        '
+        Me.ErrorProvider4.ContainerControl = Me
+        '
+        'dtpFechaNacimiento
+        '
+        Me.dtpFechaNacimiento.CalendarFont = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dtpFechaNacimiento.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dtpFechaNacimiento.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.dtpFechaNacimiento.Location = New System.Drawing.Point(294, 156)
+        Me.dtpFechaNacimiento.Name = "dtpFechaNacimiento"
+        Me.dtpFechaNacimiento.Size = New System.Drawing.Size(249, 26)
+        Me.dtpFechaNacimiento.TabIndex = 13
+        Me.dtpFechaNacimiento.Value = New Date(2023, 2, 23, 1, 31, 34, 0)
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.BackColor = System.Drawing.SystemColors.InactiveCaption
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TextBox3)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.dtpFechaNacimiento)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.dgvDatosFormulario)
+        Me.Controls.Add(Me.txtNumeroTelefono)
+        Me.Controls.Add(Me.btnEnviarDatos)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ErrorProvider2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ErrorProvider3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.dgvDatosFormulario, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ErrorProvider4, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -116,8 +259,22 @@ Partial Class Form1
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents TextBox3 As TextBox
-    Friend WithEvents Button1 As Button
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents txtNumeroTelefono As TextBox
+    Friend WithEvents btnEnviarDatos As Button
+    Friend WithEvents ErrorProvider1 As ErrorProvider
+    Friend WithEvents ErrorProvider2 As ErrorProvider
+    Friend WithEvents ErrorProvider3 As ErrorProvider
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtCantidadContactos As TextBox
+    Friend WithEvents ErrorProvider4 As ErrorProvider
+    Friend WithEvents btnCantidadContactos As Button
+    Friend WithEvents dgvDatosFormulario As DataGridView
+    Friend WithEvents ID As DataGridViewTextBoxColumn
+    Friend WithEvents NOMBRE As DataGridViewTextBoxColumn
+    Friend WithEvents NumeroDeTelefono As DataGridViewTextBoxColumn
+    Friend WithEvents FechaDeNacimiento As DataGridViewTextBoxColumn
+    Friend WithEvents Edad As DataGridViewTextBoxColumn
+    Friend WithEvents dtpFechaNacimiento As DateTimePicker
 End Class
--- WindowsForms_VB/Form1.vb
+++ WindowsForms_VB/Form1.vb
@@ -2,19 +2,90 @@
 Imports System.Windows.Forms.VisualStyles.VisualStyleElement
 
 Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private nn As Integer = 0
+    Private index As Integer = 0
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Panel1.Visible = True
+        dgvDatosFormulario.Visible = False
+    End Sub
+    ' este boton es el que envia los datos al DataGridView
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnEnviarDatos.Click
+        Dim personas(nn) As Contacto
+        Dim cantidad As Integer = CInt(txtCantidadContactos.Text)
+
         Try
-            Dim x As New Contacto()
-            x.Nombre = TextBox1.Text
-            x.Telefono = TextBox2.Text
-            x.FechaNacimiento = DateTime.Parse(TextBox3.Text)
-            MsgBox(x.ToString(), MsgBoxStyle.Information)
+            If Not txtNombre.Text = Nothing Or Not txtNumeroTelefono.Text = Nothing Then
+                Dim person = New Contacto With
+               {
+               .Nombre = txtNombre.Text,
+               .Telefono = txtNumeroTelefono.Text,
+               .FechaNacimiento = dtpFechaNacimiento.Text
+               }
+                personas(nn) = person
+
+                Me.dgvDatosFormulario.Rows.Add(nn + 1, UCase(personas(nn).Nombre), personas(nn).Telefono, personas(nn).FechaNacimiento, personas(nn).Edad)
+                nn += 1
+            End If
+
+            ' validaciones que generan un ErrorProvider
+            If String.IsNullOrEmpty(txtNombre.Text) Or txtNombre.Text = Nothing Then
+                ErrorProvider1.SetError(txtNombre, "Debe ingresar un nombre.")
+            End If
+            If String.IsNullOrEmpty(txtNumeroTelefono.Text) Or txtNumeroTelefono.Text = Nothing Then
+                ErrorProvider2.SetError(txtNumeroTelefono, "Debe ingresar un numero.")
+            End If
+            'If String.IsNullOrEmpty(dtpFechaNacimiento.Text) Then
+            '    ErrorProvider3.SetError(dtpFechaNacimiento, "Debe ingresar esta fecha en el formato correcto.")
+            'End If
+            ' mensage de advertencia por los errores al rellenar los textbox/datos
+            If (txtNombre.Text = Nothing Or txtNumeroTelefono.Text = Nothing) Then
+                MessageBox.Show("Debe ingresar todos los datos correctamente.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+        Catch ex As Exception
+
+
+        End Try
+        ' validaciones para remover los ErrorProvider y desactivar boton txtEnviarDatos
+        If Not String.IsNullOrWhiteSpace(txtNombre.Text) Then
+            ErrorProvider1.Clear()
+        End If
+        If Not String.IsNullOrWhiteSpace(txtNumeroTelefono.Text) Then
+            ErrorProvider2.Clear()
+        End If
+        'If Not String.IsNullOrWhiteSpace(dtpFechaNacimiento.Text) Then
+        '    ErrorProvider3.Clear()
+        'End If
+        If nn = index Then
+            btnEnviarDatos.Enabled = False
+        End If
+        txtNombre.Clear()
+        txtNumeroTelefono.Clear()
+        dtpFechaNacimiento.ResetText()
+
+    End Sub
+    ' este boton es el que define la cantidad de veces que se va a instanciar la clase Contacto en un arreglo de clase
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnCantidadContactos.Click
+
+        Try
+            index = txtCantidadContactos.Text
+            btnEnviarDatos.Enabled = True
+            dgvDatosFormulario.Visible = True
 
         Catch ex As Exception
-            If (TextBox1.Text = Nothing Or TextBox2.Text = Nothing Or TextBox3.TextLength < 8 Or TextBox3.Text = Nothing) Then
-                MsgBox("DEBES RESPETAR EL FORMATO Y NO DEJAR DATOS SIN LLENAR", MsgBoxStyle.Exclamation)
+            If Not IsNumeric(txtCantidadContactos.Text) Or IsNothing(txtCantidadContactos.Text) Then
+                ErrorProvider4.SetError(txtCantidadContactos, "Debe ingresar un numero.")
+                MessageBox.Show("Debe ingresar un numero.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
 
             End If
         End Try
+        If IsNumeric(txtCantidadContactos.Text) Then
+            ErrorProvider4.Clear()
+            Panel1.Visible = False
+            txtCantidadContactos.Visible = False
+            Label5.Visible = False
+            btnCantidadContactos.Visible = False
+        End If
+
     End Sub
+
 End Class
